c8f83f335e9616b7d2c7b30412b8327927c51c57
Derek Schmitt <derek@Dereks-MacBook-Pro-2.local>
2022-07-24 19:11:12 -0400

    Add exception handeling to login request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -17,15 +17,20 @@ class AuthController extends Controller
             'password' => ['required', 'min:6', 'max:30']
         ]);
 
-        // todo add error handling
-        $response = Http::asForm()->post(config('auth.oauth.base_url') . '/oauth/token', [
-            'grant_type' => config('auth.oauth.grant_type'),
-            'client_id' => config('auth.oauth.client_id'),
-            'client_secret' => config('auth.oauth.client_secret'),
-            'username' => $request->get('email'),
-            'password' => $request->get('password'),
-            'scope' => '*',
-        ]);
+        try {
+            $response = Http::asForm()->post(config('auth.oauth.base_url') . '/oauth/token', [
+                'grant_type' => config('auth.oauth.grant_type'),
+                'client_id' => config('auth.oauth.client_id'),
+                'client_secret' => config('auth.oauth.client_secret'),
+                'username' => $request->get('email'),
+                'password' => $request->get('password'),
+                'scope' => '*',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
 
         return $response->json();
     }
